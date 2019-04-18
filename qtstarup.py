919f1d554b9cb67c2a627002658d5cbba73a335f
Rubens Zanatta <rubenszanatta@hotmail.com>
2019-04-18 08:57:36 -0300

    Commit inicial do projeto em Qt, junto das imagens básicas e update do .rpd com novo endereço.

---
+++ qtstarup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''
+GUI DE INICIO DE SESSÃO PARA RASPBERRY PIs DO LABUFSC
+
+Rodando no magnífico PyQt.
+Requisitos de instalação: Python 3.X, PyQt5
+
+'''
+
+#--------------------
+# IMPORTS
+#--------------------
+
+from PyQt5.QtWidgets  import *
+from PyQt5.QtGui import QIcon, QImage, QPalette, QBrush
+from PyQt5.QtCore import QSize
+
+
+import subprocess
+
+
