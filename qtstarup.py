5fb70cfe9e5444a253657f2ae2b0b7f379b90f8a
Rubens Zanatta <rubenszanatta@hotmail.com>
2019-04-23 10:43:27 -0300

    adicao dos botoes e layout inicias

--- qtstarup.py
+++ qtstarup.py
@@ -12,7 +12,7 @@ Requisitos de instalação: Python 3.X, PyQt5
 # IMPORTS
 #--------------------
 
-from PyQt5.QtWidgets  import *
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QImage, QPalette, QBrush
 from PyQt5.QtCore import QSize
 
@@ -25,6 +25,33 @@ class MainWindow(QWidget):
         QWidget.__init__(self)
         self.setGeometry(widthMain, widthMain, heightMain, heightMain)
 
+        ## ------------ elementos ----------
+        ## ------ botao 1
+        button1 = QPushButton()
+        button1.setGeometry(250, 250, 300, 300)
+        icon1 = QIcon('resources/ubuntu_button.png')
+        button1.setIcon(icon1)
+        button1.setIconSize(QSize(200, 250))
+
+        ## ------ botao 2
+        button2 = QPushButton()
+        button2.setGeometry(250, 250, 300, 300)
+        icon2 = QIcon('resources/windows_button.png')
+        button2.setIcon(icon2)
+        button2.setIconSize(QSize(200, 250))
+
+        ## ------ botao 3 (not yet)
+        #button3 = QPushButton()
+
+
+        ## ----------- layout ----------
+        layoutbuttons = QHBoxLayout()
+        layoutbuttons.addWidget(button1)
+        layoutbuttons.addWidget(button2)
+        self.setLayout(layoutbuttons)
+
+        ## ----------- voila ----------
+        self.show()
 
 
 if __name__ == "__main__":
