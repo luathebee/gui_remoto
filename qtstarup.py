1bb7213ec95b79e6cc4b247c3cf8292c962febad
Rubens Zanatta <rubenszanatta@hotmail.com>
2019-04-25 13:54:00 -0300

    formatacao de texto(stylesheet) adicionado alem de algumas magens no layout da coluna1

--- qtstarup.py
+++ qtstarup.py
@@ -14,7 +14,7 @@ Requisitos de instalação: Python 3.X, PyQt5
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QImage, QPalette, QBrush
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QSize , Qt
 
 
 import subprocess, sys
@@ -24,6 +24,7 @@ class MainWindow(QWidget):
     def __init__(self, widthMain, heightMain):
         QWidget.__init__(self)
         self.setGeometry(widthMain, widthMain, heightMain, heightMain)
+        self.setMinimumSize(QSize(700,700))
 
         ## ------------ background ---------
         oImage = QImage("resources/bkgd.png")
@@ -47,8 +48,10 @@ class MainWindow(QWidget):
         button2.setIcon(icon2)
         button2.setIconSize(QSize(250, 300))
 
-        ## ------ botao 3 (not yet)
-        #button3 = QPushButton()
+        ## ------ header 1
+        header1 = QLabel('Seja bem vindo! \n Escolha uma das opções abaixo para acessar o sistema:')
+        header1.setAlignment(Qt.AlignCenter)
+        header1.setStyleSheet('color: white; font-size: 25px')
 
 
         ## ----------- layout ----------
@@ -57,7 +60,15 @@ class MainWindow(QWidget):
         layoutbuttons.setSpacing(25)
         layoutbuttons.addWidget(button1, 100)
         layoutbuttons.addWidget(button2, 100)
-        self.setLayout(layoutbuttons)
+
+        layoutcoluna1 = QVBoxLayout()
+        #layoutcoluna1.setSpacing(25)
+        layoutcoluna1.setContentsMargins(0, 200, 0, 200)
+        layoutcoluna1.addWidget(header1)
+        layoutcoluna1.addLayout(layoutbuttons)
+
+
+        self.setLayout(layoutcoluna1)
 
         ## ----------- voila ----------
         self.show()
